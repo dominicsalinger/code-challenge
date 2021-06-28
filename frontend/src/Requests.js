d88824018dd613c763a3bacf4c34bdd7566ea75c
Dominic Pearson <dominicsalinger@mac.com>
2021-06-28 02:27:19 -0700

    update to ssl domain

--- frontend/src/Requests.js
+++ frontend/src/Requests.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://codechallenge.net'
+const baseUrl = 'https://django.codechallenge.net'
 // const baseUrl = 'http://127.0.0.1:8000'
 
 const createProduct = async (name, description, price, quantity) => {
